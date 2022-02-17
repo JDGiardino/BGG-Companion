75060e48e5cffbf553475a56d6258c73625017f2
JDGiardino <JDGiardino@gmail.com>
2022-02-17 08:46:29 -0500

    Add error handling, caching, xmltodict, json return

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, request
+import json
+import dataclasses
+from flask import Flask, request, Response, abort
+from typing import Union
 from src.bgg_companion_api import get_random_game
+from src.exceptions import UserIsNoneError
 
 app = Flask(__name__)
 # Run this by poetry run flask run
 
+
 @app.route("/random_game")
-def post_random_game():
+def post_random_game() -> Union[str, Response]:
     args = request.args
-    if args.get('user') is None:  # add exception handling for empty strings AND for rate limits
+    if args.get('user') is None or args.get('user') == '':
         user = 'JDGiardino'
     else:
         user = args.get('user')
-    return get_random_game(user)  # return proper json rather than a string
+    try:
+        return json.dumps(dataclasses.asdict(get_random_game(user)))  # use flask's json-ify
+    except UserIsNoneError as exc:
+        abort(Response(response=str(exc), status=404))
---
+++ src/exceptions.py
@@ -0,0 +1,2 @@
+class UserIsNoneError(Exception):
+    """Raised when a user is passed that cannot be found by BoardGameGeek API"""
