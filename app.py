1e84e6ac6126064e569a1b0619c4ea4c7b1a60f7
Joe Giardino <JDGiardino@gmail.com>
2022-06-26 11:26:32 -0400

    Store cookie names as global variable

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ from typing import Union
 
 app = Flask(__name__)
 # Run this by poetry run flask run
+USERNAME_COOKIE_NAME = "username"
+VISIT_COUNT_COOKIE_NAME = "visit-count"
 
 
 @app.route("/random_game")
@@ -28,7 +30,7 @@ def post_random_game_from_users_collection() -> Union[str, Response]:
     try:
         filtered_board_games = bgg_companion_api.get_users_filtered_board_games(user)
         resp = jsonify(dataclasses.asdict(random.choice(filtered_board_games)))
-        resp.set_cookie(key="username", value=user)
+        resp.set_cookie(key=USERNAME_COOKIE_NAME, value=user)
         return resp
     except UserIsNoneError as exc:
         return abort(Response(response=str(exc), status=404))
@@ -38,18 +40,18 @@ def post_random_game_from_users_collection() -> Union[str, Response]:
 
 @app.route("/page_visit_counter")
 def page_visit_counter():
-    count = int(request.cookies.get("visit-count", 0))
+    count = int(request.cookies.get(VISIT_COUNT_COOKIE_NAME, 0))
     count += 1
     message = f"Recently, you've visited BGG-Companion " + str(count) + " times!"
 
     resp = make_response(message)
-    resp.set_cookie(key="visit-count", value=str(count))
+    resp.set_cookie(key=VISIT_COUNT_COOKIE_NAME, value=str(count))
     return resp
 
 
 @app.route("/last_username")
 def last_username():
-    return request.cookies.get("username", "")
+    return request.cookies.get(USERNAME_COOKIE_NAME, "")
 
 
 @app.route("/home")
