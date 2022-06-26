75b6ea3b9b4edc28ab3d226d0aff72f99692057c
Joe Giardino <JDGiardino@gmail.com>
2022-06-26 10:17:42 -0400

    Overwrite page-visits paragraph instead of append

--- static/app.js
+++ static/app.js
@@ -45,7 +45,7 @@ async function page_visits(){
             if (response.ok) {
                 const page_visits = await response.text();
                 const page_visits_field = document.getElementById("page-visits");
-                page_visits_field.innerHTML += page_visits;
+                page_visits_field.innerHTML = page_visits;
             } else {
                 const error_field = document.getElementById("page-visits");
                 error_field.innerText = await response.text();
