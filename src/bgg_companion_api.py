93371c3f337ce2a57a34e45490e37f2680905fc9
Joe Giardino <JDGiardino@gmail.com>
2022-07-06 23:37:27 -0400

    Comment out dev function

--- src/bgg_companion_api.py
+++ src/bgg_companion_api.py
@@ -115,6 +115,6 @@ class BggCompanionApi(object):
 
 
 # LEAVE BELOW COMMENTED : Used for development testing
-if __name__ == "__main__":
-    bgg_companion_api = BggCompanionApi(request_client=RequestsRetryClient())
-    print(bgg_companion_api.get_users_filtered_board_games("MeeplesOnTheGround"))
+# if __name__ == "__main__":
+#     bgg_companion_api = BggCompanionApi(request_client=RequestsRetryClient())
+#     print(bgg_companion_api.get_users_filtered_board_games("JDGiardino"))
