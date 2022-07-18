6ab2a89acbfaabc6567265412de88f51026421a8
Joe Giardino <JDGiardino@gmail.com>
2022-07-18 19:08:55 -0400

    Clean create table function

--- static/collection.js
+++ static/collection.js
@@ -7,58 +7,44 @@
     async function create_table(games){
         const table = document.getElementById('table')
         const header = table.createTHead();
-        const row = header.insertRow(0);
-        const cell_one = row.insertCell(0);
-        cell_one.innerText = "Game Name (released year)"
-        const cell_two = row.insertCell(1);
-        cell_two.innerText = "Overall Rank"
-        const cell_three = row.insertCell(2);
-        cell_three.innerText = "Average Rating"
-        const cell_four = row.insertCell(3);
-        cell_four.innerText = "Complexity"
-        const cell_five = row.insertCell(4);
-        cell_five.innerText = "Players"
+        const row = header.insertRow();
+        row.insertCell().innerText = "Game Name (released year)"
+        row.insertCell().innerText = "Overall Rank"
+        row.insertCell().innerText = "Average Rating"
+        row.insertCell().innerText = "Complexity"
+        row.insertCell().innerText = "Players"
         games.forEach(addGamesToTable)
 
         function addGamesToTable(game, index){
-            const row = table.insertRow(index + 1);
-            const cell_one = row.insertCell(0);
-            cell_one.innerText = `${game.name} (${game.yearpublished})`
-            const cell_two = row.insertCell(1);
-            cell_two.innerText = `${game.overallrank}`
-            const cell_three = row.insertCell(2);
-            cell_three.innerText = `${game.averagerating}`
-            const cell_four = row.insertCell(3);
-            cell_four.innerText = `${game.complexity}`
-            const cell_five = row.insertCell(4);
-            cell_five.innerText = `${game.minplayers} - ${game.maxplayers} players`
+            const row = table.insertRow();
+            row.insertCell().innerText = `${game.name} (${game.yearpublished})`
+            row.insertCell().innerText = `${game.overallrank}`
+            row.insertCell().innerText = `${game.averagerating}`
+            row.insertCell().innerText = `${game.complexity}`
+            row.insertCell().innerText = `${game.minplayers} - ${game.maxplayers} players`
         }
     }
 
     async function get_game_collection(order_by) {
         clear_table();
         const user = document.getElementById("user").value;
-        fetch(`/ordered_games?user=${user}&orderby=${order_by}`)
-            .then(async response => {
-                if (response.ok) {
-                    const games = await response.json();
-                    await create_table(games);
-                } else {
-                    const error_field = document.getElementById("game-error");
-                    error_field.innerText = await response.text();
-                }
-            });
+        const response = await fetch(`/ordered_games?user=${user}&orderby=${order_by}`)
+        if (response.ok) {
+            const games = await response.json();
+            await create_table(games);
+        } else {
+            const error_field = document.getElementById("game-error");
+            error_field.innerText = await response.text();
+        }
     }
     async function last_username(){
-          fetch('last_username')
-                .then(async response => {
-                    if (response.ok) {
-                        document.getElementById("user").value = await response.text();
-                    } else {
-                        const error_field = document.getElementById("user");
-                        error_field.innerText = await response.text();
-                    }
-                });
+        const response = await fetch ('last_username')
+        if (response.ok) {
+            document.getElementById("user").value = await response.text();
+        } else {
+            const error_field = document.getElementById("user");
+            error_field.innerText = await response.text();
+        }
     }
 
     const alphabetical_button = document.getElementById("alphabetical");
