4134757cbe70423eb147d033c663bd3696a53e0b
Joe Giardino <JDGiardino@gmail.com>
2022-07-23 15:53:32 -0400

    Add order by strings as class variables

--- src/bgg_companion_game_order.py
+++ src/bgg_companion_game_order.py
@@ -2,21 +2,26 @@ from src.models.BoardGame import BoardGame
 
 
 class OrderBoardGames:
+    ORDER_BY_ALPHABET = "alphabet"
+    ORDER_BY_RANK = "rank"
+    ORDER_BY_RATING = "rating"
+    ORDER_BY_COMPLEXITY = "complexity"
+
     def __init__(self, board_games: list[BoardGame], order_by: str):
         self.board_games = board_games
         self.order_by = order_by
 
     def order_games(self) -> list[BoardGame]:
-        if self.order_by == "alphabet":
+        if self.order_by == self.ORDER_BY_ALPHABET:
             ordered_board_games = self.__order_by_alphabet()
             return ordered_board_games
-        elif self.order_by == "rank":
+        elif self.order_by == self.ORDER_BY_RANK:
             ordered_board_games = self.__order_by_rank()
             return ordered_board_games
-        elif self.order_by == "rating":
+        elif self.order_by == self.ORDER_BY_RATING:
             ordered_board_games = self.__order_by_rating()
             return ordered_board_games
-        elif self.order_by == "complexity":
+        elif self.order_by == self.ORDER_BY_COMPLEXITY:
             ordered_board_games = self.__order_by_complexity()
             return ordered_board_games
         else:
