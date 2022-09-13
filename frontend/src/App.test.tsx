41826acf8bedff884ba3de129e063c155556bcfc
Joe Giardino <JDGiardino@gmail.com>
2022-09-13 08:47:10 -0400

    JavaScript Linter Github Action (#68)
    * JavaScript Linter Github Action
    
    * Install eslint
    
    * Remove front end directory
    
    * Add an ESLint Configuration file
    
    * Using Run ESLint GH Action
    
    * Add package.json & npx eslint in GH action
    
    * Fix typo
    
    * Fix linter & applying linter fixes
    
    * Add Prettier Formatter GH Action
    
    * Applying Prettier fixes
    
    * Fixing Prettier error

--- frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders BGG', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/BGG/i);
-  expect(linkElement).toBeInTheDocument();
-});
